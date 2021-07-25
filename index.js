2f378eeeecfaf5dc000985db90cdb6b4d5f71a9b
Hatice <haticekaraty@gmail.com>
2021-07-25 19:55:17 -0400

    commit tree 73aaa8d4ee2a095bdbaa3c3dd8ec6ebd5f5e4367

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2
+let random = Math.round(Math.random() * 100)
+//console.log(random)
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+//console.log(mod)
+let max = Math.max(1,2,3,4,20)
